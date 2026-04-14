8718ce8be7720b1462237919ffce1b2be06b7c79
Dariusz Rosiek <65536630+dariusz-rosiek@users.noreply.github.com>
2026-04-14 16:09:36 +0200

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let sorted = strs.map((e) => e.split("").sort().join(""));
+
+        let map = {};
+
+        for (let i = 0; i < sorted.length; i++) {
+            if (!map[sorted[i]]) {
+                map[sorted[i]] = [strs[i]]
+            } else {
+                map[sorted[i]].push(strs[i])
+            }
+        }
+
+        console.log(sorted)
+        console.log(map)
+        return Object.values(map)
+
+    }
+}
