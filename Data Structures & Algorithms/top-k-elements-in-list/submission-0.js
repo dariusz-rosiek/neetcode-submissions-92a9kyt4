f92f8c9599256f5e66e752e8dc1d1c91032aef89
Dariusz Rosiek <65536630+dariusz-rosiek@users.noreply.github.com>
2026-04-14 16:37:32 +0200

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const map = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            if (!map[nums[i]]) {
+                map[nums[i]] = 1;
+            } else {
+                map[nums[i]]++
+            }
+        }
+
+        return Object.entries(map).sort((a, b) => b[1] - a[1]).slice(0, k).map(([key, value]) => Number(key))
+    }
+}
