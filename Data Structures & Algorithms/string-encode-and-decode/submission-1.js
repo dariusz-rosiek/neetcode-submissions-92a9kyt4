5b7a5fc3a3884a4ee3cff793816583bbdc4e3e9a
Dariusz Rosiek <65536630+dariusz-rosiek@users.noreply.github.com>
2026-04-15 16:09:58 +0200

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.js
@@ -0,0 +1,43 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let result = "";
+
+        for (const str of strs) {
+            result += str.length + "#" + str;
+        }
+
+        return result;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        const result = [];
+        let i = 0;
+
+        while (i < str.length) {
+            let j = i;
+
+            while (str[j] !== "#") {
+                j++;
+            }
+
+            const length = Number(str.slice(i, j));
+
+            const wordStart = j + 1;
+            const wordEnd = wordStart + length;
+
+            result.push(str.slice(wordStart, wordEnd));
+
+            i = wordEnd;
+        }
+
+        return result;
+    }
+}
