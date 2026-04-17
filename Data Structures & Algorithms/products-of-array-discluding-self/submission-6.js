5e460499472eef41dc9de1a3c3bf3cf8c9c5bb6c
Dariusz Rosiek <65536630+dariusz-rosiek@users.noreply.github.com>
2026-04-17 14:26:15 +0200

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        const result = new Array(nums.length).fill(1);
+        let prefix = 1;
+        for (let i = 0; i < result.length; i++) {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+        let suffix = 1;
+        for (let i = result.length - 1; i >= 0; i--) {
+            result[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return result;
+    }
+}
