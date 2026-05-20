041261dc462c7d379de96c34a2f393d967da28fa
Dariusz Rosiek <65536630+dariusz-rosiek@users.noreply.github.com>
2026-05-20 13:32:07 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        const map = {};
+
+        for (const letter of s) {
+            if (!map[letter]) {
+                map[letter] = 1;
+            } else {
+                map[letter]++;
+            }
+
+        }
+
+        if (s.length !== t.length) return false;
+
+        for (const letter of t) {
+            if (!map[letter]) return false;
+
+            if (map[letter] < 1) return false;
+
+            map[letter]--;
+        }
+
+        return true;
+    }
+}
